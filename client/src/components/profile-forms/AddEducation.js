d885793d614157804b7fa9bf0cfa7a3f18a2ab21
Alif Munim <alif.munim@ryerson.ca>
2021-01-03 18:33:53 -0500

    Update education form

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -1,8 +1,13 @@
 import React, { Fragment, useState } from 'react'
+
+import { Form, Button, Row, Col } from "react-bootstrap";
+import FormContainer from '../layout/FormContainer';
+
 import { withRouter } from "react-router-dom";
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import { addEducation } from "../../actions/profile";
+
 
 const AddEducation = ({ addEducation, history }) => {
     const [formData, setFormData] = useState({
@@ -20,57 +25,59 @@ const AddEducation = ({ addEducation, history }) => {
     const { school, degree, fieldofstudy, from, to, current, description } = formData;
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+    const onSubmit = e => {
+        e.preventDefault();
+        addEducation(formData, history);
+    }
 
     return (
         <Fragment>
-            <h1 className="large text-primary">
-            Add An Education
-            </h1>
-            <p className="lead">
-                <i className="fas fa-code-branch"></i> Add any school or bootcamp that you have attended
-            </p>
-            <small>* = required field</small>
-            <form className="form" onSubmit={e => {
-                e.preventDefault();
-                addEducation(formData, history);
-            }}>
-                <div className="form-group">
-                    <input type="text" placeholder="* Degree" name="degree" 
-                    value={degree} onChange={e => onChange(e)} required />
-                </div>
-                <div className="form-group">
-                    <input type="text" placeholder="* School" name="school" value={school} onChange={e => onChange(e)} required />
-                </div>
-                <div className="form-group">
-                    <input type="text" placeholder="Field of Study" name="fieldofstudy" value={fieldofstudy} onChange={e => onChange(e)}/>
-                </div>
-                <div className="form-group">
-                    <h4>From Date</h4>
-                    <input type="date" name="from" value={from} onChange={e => onChange(e)}/>
-                </div>
-                <div className="form-group">
-                    <p><input type="checkbox" name="current" checked={current} value={current} onChange={e => { 
+            <FormContainer>
+                <h1>Add Education</h1>
+                <Form onSubmit={(e) => onSubmit(e)}>
+                    <Form.Group>
+                        <Form.Control type="text" placeholder="* Degree" name="degree" 
+                        value={degree} onChange={e => onChange(e)} required />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control type="text" placeholder="* School" name="school" value={school} onChange={e => onChange(e)} required />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control type="text" placeholder="Field of Study" name="fieldofstudy" value={fieldofstudy} onChange={e => onChange(e)}/>
+                    </Form.Group>
+                    <Form.Group>
+                        <h4>From Date</h4>
+                        <Form.Control type="date" name="from" value={from} onChange={e => onChange(e)}/>
+                    </Form.Group>
+                    <Form.Group>
+                    <Form.Check 
+                        label="Current Job"
+                        type="checkbox" 
+                        name="current" 
+                        checked={current} 
+                        value={current} 
+                        onChange={e => { 
                         setFormData({ ...formData, current: !current }); 
                         toggleDisabled(!toDateDisabled)
-                        }} value="" /> Current Job</p>
-                </div>
-                <div className="form-group">
-                    <h4>To Date</h4>
-                    <input type="date" name="to" value={to} onChange={e => onChange(e)} 
-                        disabled={toDateDisabled ? "disabled" : ""}/>
-                </div>
-                <div className="form-group">
-                    <textarea
-                        name="description"
-                        cols="30"
-                        rows="5"
-                        placeholder="Program Description"
-                        value={description} onChange={e => onChange(e)}
-                    ></textarea>
-                </div>
-                <input type="submit" className="btn btn-primary my-1" />
-                <a className="btn btn-light my-1" href="dashboard.html">Go Back</a>
-            </form>
+                        }} value="" />
+                    </Form.Group>
+                    <Form.Group>
+                        <h4>To Date</h4>
+                        <Form.Control type="date" name="to" value={to} onChange={e => onChange(e)} 
+                            disabled={toDateDisabled ? "disabled" : ""}/>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control
+                            as="textarea"
+                            name="description"
+                            rows={5}
+                            placeholder="Program Description"
+                            value={description} onChange={e => onChange(e)}
+                        ></Form.Control>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">Submit</Button>
+                </Form>
+            </FormContainer>
         </Fragment>
     )
 }
