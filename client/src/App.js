92fe0ff621d0a0b88caddda8d6bcbfc187099428
Alif Munim <alif.munim@ryerson.ca>
2021-01-03 14:57:22 -0500

    Use react bootstrap for forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./bootstrap.min.css"
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
