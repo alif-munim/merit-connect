17c491b6f5759a03c15869e2273dbfdce7b05529
Alif Munim <alif.munim@ryerson.ca>
2021-01-17 18:58:25 -0500

    Use react bootstrap for posts

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+
+import { Form, Button } from "react-bootstrap";
+
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { addComment } from "../../actions/post";
@@ -8,27 +11,25 @@ const CommentForm = ({ postId, addComment }) => {
     const [text, setText] = useState("");
 
     return (
-        <div className="post-form">
-            <div className="bg-primary p">
-            <h3>Leave a Comment</h3>
-            </div>
-            <form className="form my-1" onSubmit={e => {
+        <>
+        <Form className="my-3" onSubmit={e => {
                 e.preventDefault();
                 addComment(postId, { text });
                 setText("");
             }}>
-                <textarea
-                    name="text"
-                    cols="30"
-                    rows="5"
-                    placeholder="Create a post"
-                    value={text}
-                    onChange={e => setText(e.target.value)}
-                    required
-                ></textarea>
-                <input type="submit" className="btn btn-dark my-1" value="Submit" />
-            </form>
-        </div>
+        <Form.Group>
+            {/* <Form.Label>Post</Form.Label> */}
+            <Form.Control as="textarea" rows={5} 
+            placeholder="Create a post"
+            name="post"
+            value={text}
+            onChange={e => setText(e.target.value)}/>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        </>
     )
 }
 
